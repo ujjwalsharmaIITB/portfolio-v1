17711a22b8d0882a377a6dc15aca1d5e2046e877
ujjwalsharmaIITB <142598228+ujjwalsharmaIITB@users.noreply.github.com>
2023-12-13 19:29:43 +0000

    added homepage setting in vite build removed old build

--- portfolio-v1/src/App.jsx
+++ portfolio-v1/src/App.jsx
@@ -15,7 +15,7 @@ import {
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/~ujjwalsharma">
         <div className="relative z-0 bg-primary">
           <div className="bg-hero-pattern bg-cover bg-no-repeat">
             <Navbar />
